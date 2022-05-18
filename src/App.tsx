36d4112cdb6296471e4c3afd40b11aa5bed91b5d
likearts <likearts@gnifrix.com>
2022-05-18 22:30:11 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Model(props:any) {
+  const { scene } = useGLTF(
+    "https://storage.googleapis.com/webar-dev-bd3d4.appspot.com/Owner/asus/SA002-TW-21110019/Zen_UX8402_Laptop_Def_Def/Zen_UX8402_Laptop_Def_Def.gltf"
   );
+  return <primitive object={scene} />;
 }
 
-export default App;
+export default function App() {
+  return (
+    <Canvas camera={{ position: [-10, 15, 15], fov: 50 }}>
+      <ambientLight intensity={1} />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+}
